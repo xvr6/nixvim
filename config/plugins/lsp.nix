397cb92c13c44c10bbe88fab6f09dafbfa17755e
xvr6 <xvr0612@gmail.com>
2026-04-15 23:26:38 -0400

    added more lsp

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -12,7 +12,14 @@
       servers = {
         nixd.enable = true;
         vue_ls.enable = true;
+        ts_ls.enable = true;
         gopls.enable = true;
+        pylsp.enable = true;
+        java_language_server.enable = true;
+
+        jsonls.enable = true;
+        tombi.enable = true; #.toml
+        rumdl.enable = true; #.md
       };
 
       # -- KEYBINDS
@@ -24,7 +31,7 @@
           "gi" = "implementation";
           "gr" = "references";
           "gt" = "type_definition";
-        
+
           "<leader>ca" = "code_action";
           "<leader>rn" = "rename";
           "<leader>wa" = "add_workspace_folder";
